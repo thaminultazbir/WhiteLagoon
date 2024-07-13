075769112bec8848a7bdbb8a338e67fb0e11b539
thaminultazbir <thaminultazbir1999@gmail.com>
2024-07-14 00:30:48 +0600

    Villas table created and 3 data push

---
+++ WhiteLagoon.Infrastructure/Migrations/20240713181840_seedVillaToDbAndModTableColumn.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WhiteLagoon.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedVillaToDbAndModTableColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "sqft",
+                table: "Villas",
+                newName: "Sqft");
+
+            migrationBuilder.RenameColumn(
+                name: "price",
+                table: "Villas",
+                newName: "Price");
+
+            migrationBuilder.RenameColumn(
+                name: "occupancy",
+                table: "Villas",
+                newName: "Occupancy");
+
+            migrationBuilder.RenameColumn(
+                name: "UpdatedDate",
+                table: "Villas",
+                newName: "Updated_Date");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedDate",
+                table: "Villas",
+                newName: "Created_Date");
+
+            migrationBuilder.InsertData(
+                table: "Villas",
+                columns: new[] { "Id", "Created_Date", "Description", "ImageUrl", "Name", "Occupancy", "Price", "Sqft", "Updated_Date" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.", "https://placehold.co/600x400", "Royal Villa", 4, 200.0, 550, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 2, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.", "https://placehold.co/600x401", "Premium Pool Villa", 4, 300.0, 550, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 3, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.", "https://placehold.co/600x402", "Luxury Pool Villa", 4, 400.0, 750, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.RenameColumn(
+                name: "Sqft",
+                table: "Villas",
+                newName: "sqft");
+
+            migrationBuilder.RenameColumn(
+                name: "Price",
+                table: "Villas",
+                newName: "price");
+
+            migrationBuilder.RenameColumn(
+                name: "Occupancy",
+                table: "Villas",
+                newName: "occupancy");
+
+            migrationBuilder.RenameColumn(
+                name: "Updated_Date",
+                table: "Villas",
+                newName: "UpdatedDate");
+
+            migrationBuilder.RenameColumn(
+                name: "Created_Date",
+                table: "Villas",
+                newName: "CreatedDate");
+        }
+    }
+}

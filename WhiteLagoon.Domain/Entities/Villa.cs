c1e300eb4a928181c8ef05d993c93647fefa97e7
thaminultazbir <thaminultazbir1999@gmail.com>
2024-07-24 21:58:09 +0600

    Update & Delete Operation with Database and Dark theme installed.

--- WhiteLagoon.Domain/Entities/Villa.cs
+++ WhiteLagoon.Domain/Entities/Villa.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +10,15 @@ namespace WhiteLagoon.Domain.Entities
     public class Villa
     {
         public int Id { get; set; }
+        [MaxLength(50)]
         public required string Name { get; set; }
         public string? Description { get; set; }
+        [Display(Name = "Price per Night")]
+        [Range(10, 100)]
         public double Price { get; set; }
         public int Sqft { get; set; }
         public int Occupancy { get; set; }
+        [Display(Name = "Image URL")]
         public string? ImageUrl { get; set; }
         public DateTime Created_Date { get; set; }
         public DateTime Updated_Date { get; set; }
--- WhiteLagoon.Web/Controllers/VillaController.cs
+++ WhiteLagoon.Web/Controllers/VillaController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Domain.Entities;
 using WhiteLagoon.Infrastructure.Data;
 
 namespace WhiteLagoon.Web.Controllers
@@ -15,5 +16,68 @@ namespace WhiteLagoon.Web.Controllers
             var villas = _db.Villas.ToList();
             return View(villas);
         }
+
+        public IActionResult Create() 
+        { 
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Villa obj)
+        {
+            if (obj.Name == obj.Description) 
+            {
+                ModelState.AddModelError("Name", "The Name and Description must not be same");
+            }
+            if (ModelState.IsValid) 
+            {
+                _db.Villas.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Villa");
+            }
+            else { return View(obj); }
+
+        }
+        public IActionResult Update(int villaId)
+        {
+            Villa? Obj = _db.Villas.FirstOrDefault(u => u.Id == villaId);
+            if (Obj == null) { return RedirectToAction("Error", "Home"); }
+            return View(Obj);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Villa obj)
+        {
+            if (ModelState.IsValid && obj.Id > 0)
+            {
+                _db.Villas.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Villa");
+            }
+            else { return View(obj); }
+
+        }
+
+        public IActionResult Delete(int villaId)
+        {
+            Villa? Obj = _db.Villas.FirstOrDefault(u => u.Id == villaId);
+            if (Obj == null) { return RedirectToAction("Error", "Home"); }
+            return View(Obj);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Villa obj)
+        {
+            Villa? objFromDb = _db.Villas.FirstOrDefault(u => u.Id == obj.Id);
+            if (objFromDb is not null)
+            {
+                _db.Villas.Remove(objFromDb);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Villa");
+            }
+            else { return View(obj); }
+
+        }
+
     }
 }
